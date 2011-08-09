46e0f55a92e386b5745062cfbc3cf9f038f3df87
John Polacek <johnpolacek@gmail.com>
2011-08-09 12:33:59 -0500

    removed some comments/documentation, updated Modernizr to 2.0.6

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,23 +1,8 @@
 // remap jQuery to $
 (function($){})(window.jQuery);
 
-
-/* trigger when page is ready */
 $(document).ready(function (){
 
-	// your functions go here
+	// functions
 
 });
-
-
-/* optional triggers
-
-$(window).load(function() {
-	
-});
-
-$(window).resize(function() {
-	
-});
-
-*/
